2d6ef7fe92b3935b23e65d865be625d320150029
nilarjo <156613315+nilarjo@users.noreply.github.com>
2024-01-15 13:49:34 -0500

    Modified both files in he repo clone
    -Delated a print() statement from script.py
    -Added two print() statements to script2.py

--- Script1.py
+++ Script1.py
@@ -1,3 +1,2 @@
 #Lines from Zen from Python
 print("Beautiful is better than ugly")
-print("Simple is better complex")
--- Script2.py
+++ Script2.py
@@ -1,2 +1,4 @@
 #this is a good programming advice
 print("Although practicality beats purity")
+print("If the implementaton is easy to explain, it may be a good idea")
+print("Namespaces are one honking great idea -- lets do more of those")
