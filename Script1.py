3404a0f5ed5195efe3e34cc0b396a9552a0929d4
nilarjo <156613315+nilarjo@users.noreply.github.com>
2024-01-15 13:25:18 -0500

    Revert "Modified Both Scripts"
    This reverts commit 3263bc4db0b8cc8bf57a1096cb1781a617c4a411.

--- Script1.py
+++ Script1.py
@@ -1,2 +1,3 @@
 #Lines from Zen from Python
 print("Beautiful is better than ugly")
+print("Simple is better complex")
--- Script2.py
+++ Script2.py
@@ -1,3 +1,2 @@
-#Lines from Zen of Python
 print("Although practicality beats purity")
 print("Errors should never pass silently.")
